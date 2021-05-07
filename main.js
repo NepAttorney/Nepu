bc130d3704ac71f526200d667033c8fe7849a601
NepAttorney <61463355+NepAttorney@users.noreply.github.com>
2021-05-08 00:09:27 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+require('module-alias/register');
+
+const fs = require('fs');
+
+const Discord = require('discord.js');
+
+require('dotenv').config();
+
+const client = new Discord.Client({ partials: [ "MESSAGE", "REACTION", "CHANNEL"] });
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+client.categories - fs.readdirSync('./commnads/');
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+});
+
+client.once('ready', () => {
+    client.user.setActivity('Nep nep nep nep', { type: "CUSTOM_STATUS" });
+});
+
+client.login(process.env.DISCORD_TOKEN);
