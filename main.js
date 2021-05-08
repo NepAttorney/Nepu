962d1cbea8480bac0945fd4ee2385b3a64cde827
NepAttorney <61463355+NepAttorney@users.noreply.github.com>
2021-05-09 00:17:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-require('module-alias/register');
+require("module-alias/register");
 
-const fs = require('fs');
+const fs = require("fs");
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const client = new Discord.Client({ partials: [ "MESSAGE", "REACTION", "CHANNEL"] });
+const client = new Discord.Client({
+  partials: [ "MESSAGE", "REACTION", "CHANNEL" ],
+});
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
-client.categories - fs.readdirSync('./commands/');
+client.categories - fs.readdirSync("./commands/");
 
-['command_handler', 'event_handler'].forEach(handler => {
-    require(`./handlers/${handler}`)(client, Discord);
+["command_handler", "event_handler"].forEach((handler) => {
+  require(`./handlers/${handler}`)(client, Discord);
 });
 
-client.once('ready', () => {
-    client.user.setActivity('Nep nep nep nep', { type: "CUSTOM_STATUS" });
+client.once("ready", () => {
+  client.user.setActivity("Nep nep nep nep", { type: "CUSTOM_STATUS" });
 });
 
 client.login(process.env.DISCORD_TOKEN);
