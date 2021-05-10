c0afb9abe0e3ee3bff82259fb61d031d0a6b1b08
NepAttorney <61463355+NepAttorney@users.noreply.github.com>
2021-05-10 23:33:23 +0300

    Create message.js

--- main.js
+++ main.js
@@ -37,33 +37,7 @@ client.once("ready", () => {
         .registerCommandsIn(path.join(__dirname, "commands"));
 });
 
-client.on('message', (message) => {
-    // Check if bot was pinged
-    if (message.author.bot) return false;
 
-    if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
-
-    if (message.mentions.has(client.user.id)) {
-        const pingedMessages = [
-            "Nep!",
-            "Imma nep you up!",
-            "Nepu!",
-            "Nepu?",
-            "Nepu nepu nepu!",
-            "Sounds cool!",
-            "Coolio.",
-            "Who's next on the nep list?",
-            "Is it pudding time?",
-            "Pudding! Pudding! Pudding!",
-            "Blepu~",
-            "*Happy Nep noises*"
-        ];
-
-        const pingedMessage = pingedMessages[Math.floor(Math.random() * pingedMessages.length)];
-
-        message.channel.send(pingedMessage);
-    };
-});
 const logChannel = '840335142644219914'
 
 client.on('guildCreate', (guild) => {
