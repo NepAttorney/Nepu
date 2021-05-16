eb8fb8b2c037af0717c167b031f5a01c7e0a0390
NepAttorney <61463355+NepAttorney@users.noreply.github.com>
2021-05-16 03:02:37 +0300

    changelog command

---
+++ commands/commands/changelog.js
@@ -0,0 +1,48 @@
+const Commando = require('discord.js-commando');
+const commonTags = require('common-tags');
+
+const changeLog = [
+    {
+        version: "1.1.0",
+        changes: commonTags.stripIndents`Fixed some bugs
+        Fun group added.
+        Deleted the ability to make suggestions channels.
+        Removed an unneeded package.`
+    },
+    {
+        version: "1.0.5",
+        changes: commonTags.stripIndents`Added a database.
+        Can set a suggestions channel where every message will be an embed.
+        Util group added.`
+    },
+    {
+        version: "1.0.0",
+        changes: commonTags.stripIndents`First version!`
+    }
+]
+
+module.exports = class ChangeLogCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "changelog",
+            group: "commands",
+            memberName: "changelog",
+            description: "See the changelog!",
+            hidden: true,
+            ownerOnly: true
+        })
+    }
+
+    /**
+     * @param {Commando.CommandoMessage} message 
+     */
+    async run(message) {
+        let changesMessage = ""
+        for (const change of changeLog) {
+            const { version, changes } = change
+            changesMessage += `**Version:** ${version}\n${changes}\n\n`
+        }
+
+        message.channel.send(changesMessage)
+    }
+}
